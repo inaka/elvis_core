592a5d811b9a6f77c8314b61271a8b491b4d79c9
Brujo Benavides <elbrujohalcon@inaka.net>
2016-05-02 17:48:12 +0200

    [Fix inaka/elvis#362] Correctly distinguish macros from variables (#50)
    * [#362] [inaka/elvis#362] Adding variable naming convention to erl_files ruleset
    
    * [#362] [inaka/elvis#362] Add test
    
    * [#362] Remove comments that showed up when tests passed
    
    * [#362] [Fix inaka/elvis#362] Correctly distinguish macros from variables
    
    * [#362] Fix mixed style issue

--- test/examples/pass_variable_naming_convention.erl
+++ test/examples/pass_variable_naming_convention.erl
@@ -1,6 +1,7 @@
 -module(pass_variable_naming_convention).
 
 -export([should_pass/5]).
+-export([should_pass/0, should_pass/1]).
 
 %% CamelCase must be used for variables. Don’t
 %% separate words in variables with _.
@@ -12,5 +13,10 @@ should_pass(Should, Pass, Way2Home, Fun1, Fun2) ->
     Should = "Should",
     Pass = "Pass",
     Way2Home = "Way to home",
-    Fun1 = Should ++ Pass,
+Fun1 = Should ++ Pass,
     Fun2 = Fun1 ++ Way2Home.
+
+should_pass() ->
+  ?MODULE_STRING.
+
+should_pass(_) -> ?MODULE_STRING.
