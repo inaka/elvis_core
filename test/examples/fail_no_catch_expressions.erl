97f83d5c38fb4f35f9c140fa0e2abb24857d97bc
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2022-04-27 12:04:40 +0100

    Add catch as atom, string and inside a comment
    ... for good measure :)

--- test/examples/fail_no_catch_expressions.erl
+++ test/examples/fail_no_catch_expressions.erl
@@ -3,7 +3,7 @@
 -export([catchf/0, try_catch/0, mixem/0]).
 
 catchf() ->
-    F = fun (a) -> ok end,
+    F = fun (a) -> "catch" end,
     catch F(b).
 
 try_catch() ->
@@ -12,7 +12,7 @@ try_catch() ->
         F(b)
     catch
         _ ->
-            no_prob,
+            'catch', % and there's catch inside a comment
             catchf()
     end.
 
