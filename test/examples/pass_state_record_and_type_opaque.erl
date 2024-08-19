f7d4119fdaf3fd2a5f5d545d3886f39522122ab5
belltoy <belltoy@gmail.com>
2024-08-19 15:34:34 +0800

    Simplify test cases, make them as realistic as possible

--- test/examples/pass_state_record_and_type_opaque.erl
+++ test/examples/pass_state_record_and_type_opaque.erl
@@ -7,10 +7,7 @@
 -export([
          init/1,
          handle_call/3,
-         handle_cast/2,
-         handle_info/2,
-         code_change/3,
-         terminate/2
+         handle_cast/2
         ]).
 
 -export_type([state/0]).
@@ -23,22 +20,10 @@
 init(_Args) ->
     #state{}.
 
--spec handle_call(term(), term(), term()) -> ok.
-handle_call(_Request, _From, _State) ->
-    ok.
+-spec handle_call(term(), term(), state()) -> {noreply, state()}.
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
 
--spec handle_cast(term(), term()) -> ok.
-handle_cast(_Request, _State) ->
-    ok.
-
--spec handle_info(term(), term()) -> ok.
-handle_info(_Info, _State) ->
-    ok.
-
--spec code_change(term(), term(), term()) -> term().
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
--spec terminate(term(), term()) -> ok.
-terminate(_Reason, _State) ->
-    ok.
+-spec handle_cast(term(), state()) -> {noreply, state()}.
+handle_cast(_Request, State) ->
+    {noreply, State}.
