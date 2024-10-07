f8dec952edba66ba21d01181ec8743d99c89bf8a
Milán Bór <milanbor40@gmail.com>
2024-10-07 17:42:33 +0200

    delete print

--- test/style_SUITE.erl
+++ test/style_SUITE.erl
@@ -1437,12 +1437,8 @@ verify_no_init_lists(Config) ->
 
     FailPath = "fail_verify_no_init_lists." ++ Ext,
 
-    [#{message := M, info := I}] =
+    [_] =
         elvis_core_apply_rule(Config, elvis_style, no_init_lists, #{}, FailPath),
-
-    io:format(user, "-------------------~n", []),
-    io:format(user, M, I),
-    io:format(user, "-------------------~n", []),
 
     PassPath = "pass_verify_no_init_lists." ++ Ext,
 
