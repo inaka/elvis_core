6be281face7ba131688da3880214b9399855fbb9
Bór Milán <71042542+bormilan@users.noreply.github.com>
2024-10-16 08:12:15 +0200

    New `elvis_style` rule: `max_function_clause_length` (#365)
    add max_function_clause_length rule

---
+++ test/examples/function_clause_numbers.erl
@@ -0,0 +1,50 @@
+-module(function_clause_numbers).
+
+-export([clause/1]).
+
+clause(1) ->
+  "ok";
+clause(2) -> "ok";
+clause(3) -> "ok";
+clause(4) -> "ok";
+clause(5) ->
+  "ok";
+clause(6) ->
+  "ok";
+clause(7) ->
+  "ok";
+clause(8) -> "ok";
+clause(9) -> "ok";
+clause(10) -> "ok";
+clause(11) ->
+  "ok";
+clause(12) -> "ok";
+clause(13) ->
+  "ok";
+clause(14) -> "ok";
+clause(15) -> "ok";
+clause(16) -> "ok";
+clause(17) -> "ok";
+clause(18) -> "ok";
+clause(19) ->
+  "ok";
+clause(20) ->
+  "ok";
+clause(21) ->
+  "ok";
+clause(22) -> "ok";
+clause(23) -> "ok";
+clause(24) -> "ok";
+clause(25) -> "ok";
+clause(26) -> "ok";
+clause(27) -> "ok";
+clause(28) -> "ok";
+clause(29) ->
+  "ok";
+clause(30) ->
+  "ok";
+clause(31) ->
+  "ok";
+clause(32) -> "ok";
+clause(33) ->
+  "ok".
