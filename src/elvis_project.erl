f7b8dcef223d0e59183f431b80a2b0c4c06b75d9
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-11-10 12:59:23 +0000

    Allow tag and ref in git_subdir, as per rebar3

--- src/elvis_project.erl
+++ src/elvis_project.erl
@@ -138,6 +138,13 @@ is_hex_dep(_) ->
 %% @private
 is_not_git_dep({_AppName, {_SCM, Url, _Branch}}, Regex) ->
     nomatch == re:run(Url, Regex, []);
+is_not_git_dep({_AppName,
+                {git_subdir, Url, {BranchTagOrRefType, _BranchTagOrRef}, _SubDir}},
+               Regex)
+    when BranchTagOrRefType =:= branch;
+         BranchTagOrRefType =:= tag;
+         BranchTagOrRefType =:= ref ->
+    nomatch == re:run(Url, Regex, []);
 %% Specific to plugin rebar_raw_resource
 is_not_git_dep({AppName, {raw, DepResourceSpecification}}, Regex) ->
     is_not_git_dep({AppName, DepResourceSpecification}, Regex);
@@ -147,8 +154,6 @@ is_not_git_dep({_AppName, {_SCM, Url}}, Regex) ->
 is_not_git_dep({_AppName, _Vsn, {_SCM, Url}}, Regex) ->
     nomatch == re:run(Url, Regex, []);
 is_not_git_dep({_AppName, _Vsn, {_SCM, Url, _Branch}}, Regex) ->
-    nomatch == re:run(Url, Regex, []);
-is_not_git_dep({_AppName, {git_subdir, Url, {branch, _Branch}, _SubDir}}, Regex) ->
     nomatch == re:run(Url, Regex, []).
 
 %% @private
