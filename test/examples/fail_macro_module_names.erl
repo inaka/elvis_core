e93a7c9c699ab41749c1f83ab0dec3b3537cb8d8
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2025-06-09 03:08:19 +0100

    Add report code to check that currently we don't have an issue

--- test/examples/fail_macro_module_names.erl
+++ test/examples/fail_macro_module_names.erl
@@ -6,7 +6,8 @@
          module_name/0,
          function_name/0,
          no_errors/0,
-         build_binary/0
+         build_binary/0,
+         no_errors2/0
         ]).
 
 -define(FUN_NAME, function_name).
@@ -14,6 +15,9 @@
 -define(BINARY_SIZE, 3).
 -define(function_name, function_name).
 -define(module_name, ?MODULE).
+
+-define(MATCH_MY_ERROR, {my_error, _, _}).
+-define(MATCH_MY_ERROR(ErrorTag), {my_error, ErrorTag, _}).
 
 module_name() ->
     ?MODULE:function_name(),
@@ -40,3 +44,14 @@ build_binary() ->
 
 no_errors() ->
     ?LINE.
+
+no_errors2() ->
+    try other:my_logick()
+    catch
+        throw:?MATCH_MY_ERROR(bad_auth) ->
+            handle_auth_error;
+        throw:?MATCH_MY_ERROR(input_validation) ->
+            handle_validation_error;
+        throw:?MATCH_MY_ERROR ->
+            handle_other_application_errors
+    end.
