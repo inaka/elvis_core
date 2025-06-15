bcad44d8142fbd1daa286bbedfd887637c259525
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2025-06-15 16:43:34 +0100

    Move generic test code to test utilities' module

--- test/elvis_test_utils.erl
+++ test/elvis_test_utils.erl
@@ -1,6 +1,6 @@
 -module(elvis_test_utils).
 
--export([config/0, config/1, find_file/2]).
+-export([config/0, config/1, find_file/2, elvis_core_apply_rule/5]).
 
 -spec config() -> elvis_config:configs().
 config() ->
@@ -19,4 +19,20 @@ find_file(Dirs, Pattern) ->
             {error, enoent};
         [Path | _] ->
             {ok, Path}
+    end.
+
+elvis_core_apply_rule(Config, Module, Function, RuleConfig, Filename) ->
+    ElvisConfig =
+        elvis_test_utils:config(
+            proplists:get_value(group, Config, erl_files)
+        ),
+    SrcDirs = elvis_config:dirs(ElvisConfig),
+    {ok, File} = elvis_test_utils:find_file(SrcDirs, Filename),
+    {[RulesResults], _, _} =
+        elvis_core:apply_rule({Module, Function, RuleConfig}, {[], ElvisConfig, File}),
+    case RulesResults of
+        #{error_msg := Msg, info := Info} ->
+            ct:fail(Msg, Info);
+        #{items := Items} ->
+            Items
     end.
