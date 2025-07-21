8d4b07dd49b51620c617a85b09fc4acd2bd91f3f
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2025-07-21 18:07:50 +0100

    Remove extraneous ~n

--- src/elvis_code.erl
+++ src/elvis_code.erl
@@ -189,6 +189,6 @@ print_node(#{type := Type} = Node, CurrentLevel) ->
     Indentation = lists:duplicate(CurrentLevel * 4, $\s),
     Content = ktn_code:content(Node),
 
-    ok = elvis_utils:info("~s - [~p] ~p~n", [Indentation, CurrentLevel, Type]),
+    ok = elvis_utils:info("~s - [~p] ~p", [Indentation, CurrentLevel, Type]),
     _ = lists:map(fun(Child) -> print_node(Child, CurrentLevel + 1) end, Content),
     ok.
