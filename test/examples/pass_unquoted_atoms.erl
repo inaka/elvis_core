3cc0bd99f2594e2290b4c290a89b04ae39190140
Nelson Vides <videsnelson@gmail.com>
2026-02-13 19:41:45 +0100

    Add a regression test for #545

--- test/examples/pass_unquoted_atoms.erl
+++ test/examples/pass_unquoted_atoms.erl
@@ -1,6 +1,6 @@
 -module(pass_unquoted_atoms).
 
--export([test/1, test/2]).
+-export([test/1, test/2, test_unicode/0]).
 
 test(_Test) -> ok.
 
@@ -9,3 +9,5 @@ test(_Reserved, _Words) -> ['after', 'and', 'andalso', 'band', 'begin', 'bnot', 
          'catch', 'cond', 'div', 'end', 'fun', 'if', 'let', 'not', 'of', 'or', 'orelse', 'receive',
          'rem', 'try', 'when', 'xor', 'maybe'].
 
+%% Unicode atoms that genuinely need quotes should not crash the rule (GH-545)
+test_unicode() -> 'Πλάτων'.
